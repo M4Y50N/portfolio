473b9fdc67af4052b57e95d4a808933567169bf6
pethe <pethersonreis@gmail.com>
2023-05-17 15:02:50 -0300

    update user data

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+	nameUser: "Mayson Petherson",
+	githubUser: "m4y50n",
+	linkedinUser: "mayson-petherson",
+	whatsappNumber: "+5579999633653",
+	emailUser: "pethersonreis@gmail.com",
+	instagramUser: "",
+	facebookUser: "",
 };
