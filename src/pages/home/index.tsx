dcde23771fe2df2054186e8169300968b198279f
pethe <pethersonreis@gmail.com>
2023-05-26 18:01:54 -0300

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -47,7 +47,7 @@ export const Home = (): JSX.Element => {
               <Text color="grey4">Hello, my name is {userData.nameUser}</Text>
             </Flex>
             <Text as="h1" type="heading1" color="grey5">
-            I{" "}
+              I{" "}
               <Text as="span" type="heading1" color="brand1">
                 love
               </Text>{" "}
@@ -59,7 +59,14 @@ export const Home = (): JSX.Element => {
             </Text>
             <Text type="body1" color="grey2">
               Discover here in this environment, created especially for you, all
-              my projects and technologies
+              my projects and technologies.
+              I have experience using Git and GitHub for code version control and team collaboration, as well as solid skills in JavaScript, TypeScript,
+              ReactJS and Styled Components for front-end development.
+              <br />
+              <br />
+              And i also have knowledge in Python (with Django framework) and NodeJS for creating APIs and mainly using PostgreSQL as a database.
+              I'm studying Computer Science and graduating as a Full Stack at Kenzie Academy Brasil, where I acquired skills in good programming practices,
+              Git and Git Flow, in addition to having worked on several projects and as a freelancer in the development of front-end web applications.
             </Text>
             <HeaderButtonsArea>
               <Button as="a" type="primary" href="#projects">
